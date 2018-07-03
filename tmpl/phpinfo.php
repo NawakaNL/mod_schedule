fbf6cce1949c2a7cf8971f1c103f45a7b9920e21
Tristan de Boer <info@tristandeboer.nl>
2018-07-03 19:57:27 +0200

    get locale data

--- tmpl/phpinfo.php
+++ tmpl/phpinfo.php
@@ -1,31 +1,4 @@
 <?php
-$locale_data = array();
-
-//Get locales from Linux terminal command locale
-$locales = shell_exec('locale -a');
-
-$locales = explode("n" , $locales);
-
-foreach($locales as $c => $l)
-{
-    if(strlen($l))
-    {
-        $parts = explode('.' , $l);
-        $lc = $parts[0];
-
-        list($lcode , $ccode) = explode('_' , $lc);
-
-        $lcode = strtolower($lcode);
-
-        $language = $language_codes[$lcode];
-        $country = $country_codes[$ccode];
-
-        if(strlen($language) and strlen($country))
-        {
-            $locale_data[$l] = "$language - $country - {$parts[1]}";
-        }
-    }
-}
-
-print_r($locale_data);
+$currentLocal = setlocale(LC_ALL, 0);
+echo $currentLocal;
  ?>
